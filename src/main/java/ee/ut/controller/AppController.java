1ad1c0def18373857e92c0feab4300ea0adf9be8
alandu <alan.durnev@gmail.com>
2016-02-23 22:45:00 +0200

    Project Structure Build Final v1

--- src/main/java/ee/ut/controller/AppController.java
+++ src/main/java/ee/ut/controller/AppController.java
@@ -1,25 +1,16 @@
 package ee.ut.controller;
 
 import java.util.List;
-import java.util.Locale;
-
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import ee.ut.model.Pizza;
 import ee.ut.service.PizzaService;
-
-import ee.ut.model.User;
-import ee.ut.service.UserService;
 
 
 @Controller
